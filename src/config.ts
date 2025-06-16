4efcfe2e9b79a05b0b56d94201ff60b627b577ea
Carlos Matheu Armengol <info@plastikaweb.com>
2025-06-16 10:41:13 +0200

    feat: initial setup with header, footer, layouts and basic components for blog site

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Plastikaweb | Carlos Matheu Armengol Site - Angular Freelancer';
+export const SITE_DESCRIPTION =
+  'Plastikaweb is a website for Carlos Matheu Armengol, an Angular Freelancer with more than 15 years of experience in web development.';
